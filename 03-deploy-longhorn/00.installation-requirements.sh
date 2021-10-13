206d2c37b644b5c43f0753f6a5ecd142624c6680
HieuNT <hieunt9@gmail.com>
2021-10-13 10:21:09 +0700

    update 13Oct2021

--- 03-deploy-longhorn/00.installation-requirements.sh
+++ 03-deploy-longhorn/00.installation-requirements.sh
@@ -2,7 +2,7 @@
 # Ref: https://longhorn.io/docs/1.1.0/deploy/install/#installation-requirements
 
 # 1. Installing open-iscsi
-yum install iscsi-initiator-utils
+yum install -y iscsi-initiator-utils
 
 # 2. Installing NFSv4 client
-yum install nfs-utils -y
+yum install -y nfs-utils
